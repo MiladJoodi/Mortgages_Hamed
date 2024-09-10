0539fa684fb7ce2134443777554442de38ad8f4f
Milad Joodi <winbeta.com@gmail.com>
2024-09-10 17:50:27 +0330

    Let's Start ✌

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-4xl">
+      In a digital world, we still believe in old fashioned customer values.
+    </div>
+  );
+}
