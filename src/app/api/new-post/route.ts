660d4d6dc24da0d6e18d0882aa1ead61eaa98684
Milad Joodi <winbeta.com@gmail.com>
2024-09-13 06:00:51 +0330

    up

--- src/app/api/new-post/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function(req: NextApiRequest, res: NextApiResponse){
-    if(req.method==="POST"){
-        const { title, content,imageUrl } = req.body
-        console.log(title, content,imageUrl)
-    }
-}
